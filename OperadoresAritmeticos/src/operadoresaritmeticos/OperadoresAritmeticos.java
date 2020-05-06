b423284e100da8ab31012bb8976c3214d82a8e25
Franciele Zanella <36767316+Franzanella@users.noreply.github.com>
2020-05-06 18:50:39 -0300

    OperadorAritmetico

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Fran
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       /* int n1 = 3;
+        int n2 = 5;
+        float m = (n1+n2)/2;
+        System.out.println("A media é igual a " + m);*/
+        
+        /*int numero = 10;
+        int valor = 4 + numero--;
+        System.out.println(valor);
+        System.out.println(numero);*/
+        
+        /*int x = 4;
+        x *=2;// x = x + 2
+        System.out.println(x);*/
+        
+        
+        
+        //floor arredonda para baixo
+        //ceil arredonda para cima
+        
+        /*float v = 8.9f;
+        int ar = (int) Math.ceil(v);
+        System.out.println(ar);*/
+        
+        double ale = Math.random();
+        int n = (int)(15 + ale * (50-15));
+        System.out.println(n);
+    }
+    
+    
+    
+}
